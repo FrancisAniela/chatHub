888184d019a4e8eb4af2ca714b1433d9e580cc7f
FARS <francisanielaschettino@gmail.com>
2023-01-09 15:04:00 -0400

    program

---
+++ ChatHubApi/Program.cs
@@ -0,0 +1,52 @@
+﻿using ChatHubApi;
+
+var CORSOpenPolicy = "OpenCORSPolicy";
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddSignalR();
+builder.Services.AddControllers();
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(
+      name: CORSOpenPolicy,
+      builder => {
+          builder.WithOrigins("*").AllowAnyHeader().AllowAnyMethod();
+      });
+});
+
+
+// Add services to the container.
+
+
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseCors(builder =>
+{
+    builder
+        .AllowAnyHeader()
+        .AllowAnyMethod()
+        .AllowCredentials()
+        .WithOrigins("http://localhost:4250");
+});
+
+
+
+app.UseRouting();
+app.UseAuthorization();
+app.MapHub<ChatHub>("/chatHub");
+app.UseHttpsRedirection();
+app.MapControllers();
+app.Run();
+
